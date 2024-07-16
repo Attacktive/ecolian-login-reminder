b201fd16e3efbe446176e4c5dc455e83019f19eb
Attacktive <66462458+Attacktive@users.noreply.github.com>
2024-07-16 05:25:20 +0000

    feature: implement highlighting

---
+++ src/content.ts
@@ -0,0 +1,15 @@
+const timeGrid = document.querySelector("tbody#time-grid");
+if (timeGrid) {
+	const tds = timeGrid.querySelectorAll("td");
+	for (const td of tds.values()) {
+		const textContent = td.textContent;
+		if (textContent?.includes("18홀")) {
+			td.style["backgroundColor"] = "#ffdddd";
+
+			const anchor = td.querySelector("a");
+			if (anchor) {
+				anchor.style["fontWeight"] = "666";
+			}
+		}
+	}
+}
