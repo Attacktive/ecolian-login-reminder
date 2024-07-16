5b74459b844230a0f6c6097924427244c8871df4
Attacktive <tribute2pantera@gmail.com>
2024-07-16 17:09:31 +0900

    fix: make highlighting repeat periodically

--- src/content.ts
+++ src/content.ts
@@ -1,15 +1,19 @@
-const timeGrid = document.querySelector("tbody#time-grid");
-if (timeGrid) {
-	const tds = timeGrid.querySelectorAll("td");
-	for (const td of tds.values()) {
-		const textContent = td.textContent;
-		if (textContent?.includes("18홀")) {
-			td.style["backgroundColor"] = "#ffdddd";
+const highlight = () => {
+	const timeGrid = document.querySelector("tbody#time-grid");
+	if (timeGrid) {
+		const tds = timeGrid.querySelectorAll("td");
+		for (const td of tds.values()) {
+			const textContent = td.textContent;
+			if (textContent?.includes("18홀")) {
+				td.style["backgroundColor"] = "#ffdddd";
 
-			const anchor = td.querySelector("a");
-			if (anchor) {
-				anchor.style["fontWeight"] = "666";
+				const anchor = td.querySelector("a");
+				if (anchor) {
+					anchor.style["fontWeight"] = "666";
+				}
 			}
 		}
 	}
-}
+};
+
+setInterval(highlight, 444);
