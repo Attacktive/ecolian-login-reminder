6cd5944b5bf08e41e78bf19aab396470e81059f8
Attacktive <tribute2pantera@gmail.com>
2024-08-15 21:45:49 +0900

    feature: remove unwanted entries instead of highlighting
    https://github.com/Attacktive/ecolian-login-reminder/issues/26

--- src/content.ts
+++ src/content.ts
@@ -1,19 +1,45 @@
-const highlight = () => {
+const enhanceCell = (td: HTMLTableCellElement) => {
+	const anchor = td.querySelector("a");
+	if (anchor) {
+		// in case td isn't the parent
+		const parent = anchor.parentElement;
+		if (parent) {
+			Array.from(parent.children)
+				.filter(child => child !== anchor)
+				.forEach(child => parent.removeChild(child));
+
+			anchor.style["fontSize"] = "66px";
+			anchor.style["width"] = "100%";
+			anchor.style["height"] = "88px";
+			anchor.style["lineHeight"] = "88px";
+			anchor.style["margin"] = "0";
+			anchor.style["padding"] = "0";
+		}
+	}
+};
+
+const enhanceTable = () => {
 	const timeGrid = document.querySelector("tbody#time-grid,tbody#time-grid-tablet");
 	if (timeGrid) {
+		const trs = timeGrid.querySelectorAll("tr");
+		for (const tr of trs.values()) {
+			tr.style["height"] = "88px";
+		}
+
 		const tds = timeGrid.querySelectorAll("td");
 		for (const td of tds.values()) {
 			const textContent = td.textContent;
-			if (textContent?.includes("18홀")) {
-				td.style["backgroundColor"] = "#ffdddd";
-
-				const anchor = td.querySelector("a");
-				if (anchor) {
-					anchor.style["fontWeight"] = "666";
+			const match = /(\d{1,2})\s*홀/.exec(textContent ?? "");
+			if (match) {
+				const holes = parseInt(match[1]);
+				if (holes === 18) {
+					enhanceCell(td);
+				} else if (holes === 9) {
+					td.remove();
 				}
 			}
 		}
 	}
 };
 
-setInterval(highlight, 444);
+setInterval(enhanceTable, 444);
