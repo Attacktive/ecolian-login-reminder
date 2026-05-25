b2671ec4e560f12f1d4e2a18be7ddcaf2b7f4365
Attacktive <tribute2pantera@gmail.com>
2026-05-25 20:34:20 +0900

    feature: disable style injection

--- src/content.ts
+++ src/content.ts
@@ -1,24 +1,4 @@
-const injectStyles = () => {
-	const style = document.createElement("style");
-	style.textContent = `
-		div#pc > div.reservation-list > div.list-table > table > tbody > tr {
-			height: 88px !important;
-		}
-
-		a.time-list.bnh-btn {
-			font-size: 66px !important;
-			width: 100% !important;
-			height: 88px !important;
-			line-height: 88px !important;
-			margin: 0 !important;
-			padding: 0 !important;
-		}
-	`;
-
-	document.head.appendChild(style);
-};
-
-const enhanceTable = () => {
+const remove9HoleEntries = () => {
 	const timeGrid = document.querySelector("div#pc > div.reservation-list > div.list-table > table > tbody");
 	if (timeGrid) {
 		const tds = timeGrid.querySelectorAll("td");
@@ -36,6 +16,4 @@ const enhanceTable = () => {
 	}
 };
 
-injectStyles();
-
-setInterval(enhanceTable, 444);
+setInterval(remove9HoleEntries, 444);
