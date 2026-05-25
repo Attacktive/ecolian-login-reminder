de7ae1af873576f9830922ed9c8409822a11d50a
Attacktive <tribute2pantera@gmail.com>
2026-05-25 20:20:44 +0900

    feature: change how to decide login state

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,38 +1,14 @@
-interface Site {
-	cookieUrl: string;
-	cookieName: string;
-	urlContains: string;
-	redirectTo: string;
-}
-
-const sites: Site[] = [
-	{
-		cookieUrl: "https://www.ecolian.or.kr",
-		cookieName: "sessionInfoJson",
-		urlContains: "/reser/",
-		redirectTo: "https://www.ecolian.or.kr/join/login.do"
-	}
-	// fixme: chrome.webNavigation.onBeforeNavigate event fires twice somehow. 🤷
-	/*{
-		cookieUrl: "https://www.geochang.go.kr",
-		cookieName: "sessionInfoJson",
-		urlContains: "/reser/",
-		redirectTo: "https://www.geochang.go.kr/golf/join/login.do"
-	}*/
-];
-
-sites.forEach(({ cookieUrl, cookieName, urlContains, redirectTo }) => {
-	chrome.webNavigation.onBeforeNavigate.addListener(
-		async () => {
-			const cookie = await chrome.cookies.get({ url: cookieUrl, name: cookieName });
-			if (cookie?.value) {
-				await chrome.tabs.update({ url: redirectTo });
-			}
-		},
-		{
-			url: [
-				{ urlContains }
-			]
+// fixme: chrome.webNavigation.onBeforeNavigate event fires twice somehow. 🤷
+chrome.webNavigation.onBeforeNavigate.addListener(
+	async () => {
+		const cookie = await chrome.cookies.get({ url: "https://www.geochang.go.kr", name: "refresh-token" });
+		if (!cookie?.value) {
+			await chrome.tabs.update({ url: "https://www.geochang.go.kr/golf/member/login" });
 		}
-	);
-});
+	},
+	{
+		url: [
+			{ urlContains: "/golf/reservation/reservation" }
+		]
+	}
+);
