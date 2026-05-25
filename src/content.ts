7f02a374240bce61d4d049d61e1b484c7c1f2d39
Attacktive <tribute2pantera@gmail.com>
2026-05-25 20:05:02 +0900

    feature: reflect DOM change

--- src/content.ts
+++ src/content.ts
@@ -1,31 +1,26 @@
-const enhanceCell = (td: HTMLTableCellElement) => {
-	const anchor = td.querySelector("a");
-	if (anchor) {
-		// in case td isn't the parent
-		const parent = anchor.parentElement;
-		if (parent) {
-			Array.from(parent.children)
-				.filter(child => child !== anchor)
-				.forEach(child => parent.removeChild(child));
-
-			anchor.style.fontSize = "66px";
-			anchor.style.width = "100%";
-			anchor.style.height = "88px";
-			anchor.style.lineHeight = "88px";
-			anchor.style.margin = "0";
-			anchor.style.padding = "0";
+const injectStyles = () => {
+	const style = document.createElement("style");
+	style.textContent = `
+		div#pc > div.reservation-list > div.list-table > table > tbody > tr {
+			height: 88px !important;
 		}
-	}
+
+		a.time-list.bnh-btn {
+			font-size: 66px !important;
+			width: 100% !important;
+			height: 88px !important;
+			line-height: 88px !important;
+			margin: 0 !important;
+			padding: 0 !important;
+		}
+	`;
+
+	document.head.appendChild(style);
 };
 
 const enhanceTable = () => {
-	const timeGrid = document.querySelector("tbody#time-grid,tbody#time-grid-tablet");
+	const timeGrid = document.querySelector("div#pc > div.reservation-list > div.list-table > table > tbody");
 	if (timeGrid) {
-		const trs = timeGrid.querySelectorAll("tr");
-		for (const tr of trs.values()) {
-			tr.style.height = "88px";
-		}
-
 		const tds = timeGrid.querySelectorAll("td");
 		for (const td of tds.values()) {
 			const match = /(\d{1,2})\s*홀/.exec(td.textContent ?? "");
@@ -34,13 +29,13 @@ const enhanceTable = () => {
 			}
 
 			const holes = parseInt(match[1]);
-			if (holes === 18) {
-				enhanceCell(td);
-			} else if (holes === 9) {
+			if (holes === 9) {
 				td.remove();
 			}
 		}
 	}
 };
+
+injectStyles();
 
 setInterval(enhanceTable, 444);
